969715c8721228c5b8f538a6477d9a6540d7c5bf
Ubuntu <ubuntu@ip-172-31-6-79.eu-west-1.compute.internal>
2024-03-30 10:12:56 +0000

    test2

--- file_test.py
+++ file_test.py
@@ -19,3 +19,31 @@ def test_coucou():
   # Fonction test si la résultat renvoie 'hello'
     output='hello'
     assert output == 'hello'
+
+
+def total(liste):
+    """ renvoie la somme des éléments d'une liste """
+
+    result : float = 0.0
+
+    for item in liste:
+        result += item
+
+    return (result)
+
+
+
+# Vérifie le fonctionnement de base :
+print(total([1, 2 ,3]) == 6)
+
+# Vérifie que la somme marche avec un nombre négatif et un positif :
+print(total([1, -1]) == 0)
+
+# Vérifie que la somme marche avec deux négatifs :
+print(total([-1, -1]) == -2)
+
+# Vérifie que la somme marche avec un seul élément :
+print(total([1]) == 1)
+
+# Vérifie que la liste vide renvoie bien 0 :
+print(total([]) == 0)
